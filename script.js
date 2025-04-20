99443491ccf0000ec6f6ca43828ba587ca055bb4
ClydeDean <clydesdaleog@gmail.com>
2025-04-20 11:43:41 -0400

    latest work

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Allow for selection of image to trigger modal view
+const items = document.querySelectorAll(".grid > div");
+
+items.forEach((item) => {
+  item.title = "Click to Enlarge";
+  item.addEventListener("click", () => {
+    const imgSrc = item.querySelector("img").src;
+    // Create the modal div
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    // Create the img element
+    const imgElement = document.createElement("img");
+    imgElement.src = imgSrc;
+    imgElement.alt = "Enlarged Image";
+    // Append the img element to the modal div
+    modal.appendChild(imgElement);
+    // Add the modal to the body
+    document.body.appendChild(modal);
+    // Remove the modal when it's clicked again
+    modal.addEventListener("click", () => {
+      modal.remove();
+    });
+  });
+});
+
 // Function to check if page is scrolled and adjust the logo size
 function checkScroll() {
   const navbar = document.getElementById("navbar");
